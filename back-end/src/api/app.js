1cabcedafba447ef8fa19945249e5be333cece4f
fabiano.justino <fibianojustino@gmail.com>
2022-09-20 17:15:00 -0300

    feat: add lib express-async-erros para o tratamento de erros

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+require('express-async-errors');
 
 const app = express();
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.get('/coffee', (_req, res) => res.status(418).send("ok"));
 
 module.exports = app;
--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,5 +1,5 @@
 const port = process.env.API_PORT || 3001;
 const app = require('./app');
 
-app.listen(API_PORT);
+app.listen(port);
 console.log(`Api rodando na porta ${port}`);
