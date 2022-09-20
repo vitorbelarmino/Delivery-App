4634884789f5b7f356f5b39aed447803783b394b
fabiano.justino <fibianojustino@gmail.com>
2022-09-20 17:53:10 -0300

    feat: add middleware de erro

---
+++ back-end/src/middleware/middlewareError.js
@@ -0,0 +1,20 @@
+const errorMiddleware = (err, _req, res, next) => {
+  const { name, message } = err;
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({ message });
+      break;
+    case 'UnauthorizedError':
+      res.status(401).json({ message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;    
+    default:
+      res.status(500).json({ message });
+      break;
+  }
+  console.log(err);
+  next();
+};
+export default errorMiddleware;
