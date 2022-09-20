bd01b5bb2f030ee7dbc57de74b4ba5724efc88a2
fabiano.justino <fibianojustino@gmail.com>
2022-09-20 16:54:47 -0300

    feat: add model and seeders users

---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    id: { 
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true 
+   },
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'users',
+  }); 
+
+  return User;
+};
---
+++ back-end/src/database/seeders/20220920195121-users.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkInsert('Users',
+      [{
+        id: 1,
+        displayName: 'Lewis Hamilton',
+        email: 'admin@gmail.com',
+        password: '123456',
+        role: 'admin',
+      },
+      {
+        id: 2,
+        displayName: 'Michael Schumacher',
+        email: 'seller@gmail.com',
+        password: '123456',
+        role: 'seller',
+      },
+      {
+        id: 3,
+        displayName: 'Michael Schumacher',
+        email: 'customer@gmail.com',
+        password: '123456',
+        role: 'customer',
+      },
+      ], { timestamps: false });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
