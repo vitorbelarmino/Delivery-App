77dda0eaeca89aafff0100a54244b25a7c2bb1af
fabiano.justino <fibianojustino@gmail.com>
2022-09-20 17:55:20 -0300

    feat: add import do middleware de erro na pasta base app.js

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('express-async-errors');
 const cors = require('cors');
+import errorMiddleware from './middleware/middlewareError';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(errorMiddleware);
 
 app.get('/coffee', (_req, res) => res.status(418).send("ok"));
 
