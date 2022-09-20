9b167c1bc7cc984f8e94838daf13718a17388b3a
fabiano.justino <fibianojustino@gmail.com>
2022-09-20 17:17:16 -0300

    feat: add lib cors para acesso externo à API e a lib express.json para trabalhar com requisicoes no formato json

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 require('express-async-errors');
+const cors = require('cors');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).send("ok"));
 
