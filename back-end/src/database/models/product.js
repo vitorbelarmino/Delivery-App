f7829f182d5eb170efb8bc34f16951182834a9eb
vitorbelarmino <vitor.belarmino@hotmail.com>
2022-09-21 16:27:44 -0300

    feat: :sparkles: cria nova migration, model e seed de products

---
+++ back-end/src/database/models/product.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Products = sequelize.define('Products', {
+    id: { 
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true 
+   },
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    url_image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'products',
+  }); 
+
+  return Products;
+};
