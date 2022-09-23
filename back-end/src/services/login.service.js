e6d630bdc568952c49d6b61e82a33e12a0a0a4c1
vitorbelarmino <vitor.belarmino@hotmail.com>
2022-09-22 22:14:47 -0300

    fix: :bug: adiciona senha encriptada e conserta o retorno da rota login

--- back-end/src/services/login.service.js
+++ back-end/src/services/login.service.js
@@ -1,16 +1,17 @@
 const { StatusCodes } = require('http-status-codes');
+const md5 = require('md5');
 const { createToken } = require('../auth/Jwt');
 const { Users } = require('../database/models');
 const { CustomError } = require('../helpers/customError');
 
 const loginService = async (email, password) => {
-  const user = await Users.findOne({ where: { email, password } });
+  const encryptedPassword = md5(password);
+  const user = await Users.findOne({ where: { email, password: encryptedPassword } });
+  if (!user) throw new CustomError(StatusCodes.NOT_FOUND, 'Email ou senha incorretos');
 
-  if (!user) throw new CustomError(StatusCodes.NOT_FOUND, 'Not found');
-
-  const { password: pwd, ...userWithoutPassword } = user.dataValues;
+  const { id, password: pwd, ...userWithoutPassword } = user.dataValues;
   const token = createToken(userWithoutPassword);
-  return token;
+  return { ...userWithoutPassword, token };
 };
 
 module.exports = { loginService };
