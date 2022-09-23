cac86a7b69e693c99c912b398684733f32c6be57
vitorbelarmino <vitor.belarmino@hotmail.com>
2022-09-23 18:19:10 -0300

    cria o middleware de sale

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -5,12 +5,12 @@ const router = express.Router();
 const { loginController } = require('../controllers/login.controller');
 const { registerController } = require('../controllers/register.controller');
 const { productsController } = require('../controllers/products.controller');
-const { validateLogin, validateRegister } = require('../middleware/validations');
+const { validateLogin, validateRegister, validateSale } = require('../middleware/validations');
 const sale = require('../controllers/sales.controller');
 
 router.post('/login', validateLogin, loginController)
   .post('/register', validateRegister, registerController)
   .get('/customer/products', productsController)
-  .post('/sale', sale.createSale);
+  .post('/sale', validateSale, sale.createSale);
 
 module.exports = router;
