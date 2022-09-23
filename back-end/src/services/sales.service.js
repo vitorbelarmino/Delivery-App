1f3ae544af8830bfb8893566dc7cb7d186bd57c3
vitorbelarmino <vitor.belarmino@hotmail.com>
2022-09-23 17:54:01 -0300

    adiciona validação de user e seller

--- back-end/src/services/sales.service.js
+++ back-end/src/services/sales.service.js
@@ -1,8 +1,13 @@
+const { StatusCodes } = require('http-status-codes');
 const { Users, Sales } = require('../database/models');
+const { CustomError } = require('../helpers/customError');
 
 const createSale = async (sale) => {
-  const user = await Users.findOne({ name: sale.userName });
-  const seller = await Users.findOne({ name: sale.sellerName });
+  const user = await Users.findOne({ where: { name: sale.userName } });
+  if (!user) throw new CustomError(StatusCodes.BAD_REQUEST, 'Usuário não cadastrado');
+  const seller = await Users.findOne({ where: { name: sale.SellerName } });
+  if (!seller) throw new CustomError(StatusCodes.BAD_REQUEST, 'Vendedor não cadastrado');
+
   const ojectSele = {
     userId: user.id,
     sellerId: seller.id,
